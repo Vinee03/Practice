8edf2eb073b846709ea19d4e639808d21fab6968
Vinee03 <kuttyvineetha03@gmail.com>
2016-12-03 02:14:51 -0500

    Update globals.h

--- globals.h
+++ globals.h
@@ -9,13 +9,13 @@
 
 #define READONLY_TRANSACTION    		0
 #define READWRITE_TRANSACTION   		1
-#define MISCELLANEOUS_TRANSACTION		2	//We create a pseudo transaction type called miscellaneous transction. Such transaction will contain operations such as recover, fail, dump 
+#define MISCELLANEOUS_TRANSACTION		2	 
 
-#define MAX_SITES		11	//Sites are numbered 1 to 10
-#define MAX_VARIABLES		21	//Variables are numbered from 1 to 20
+#define MAX_SITES		11	
+#define MAX_VARIABLES		21	
 
-#define ALL_VARIABLES		100	//A value indicating that operation is to be performed on all variables. Operation probably is a dump operation
-#define ALL_SITES		100	//A value indicating that operation is to be performed on all sites. Operation probably is a dump operation
+#define ALL_VARIABLES		100	
+#define ALL_SITES		100	
 
 #define READ_OPERATION		0
 #define WRITE_OPERATION		1
@@ -29,21 +29,23 @@
 #define OPERATION_PENDING	0
 #define OPERATION_BLOCKED	-1
 #define OPERATION_REJECTED	-2
-#define OPERATION_IGNORE	-3	//Indicates operation at specific is to be ignored since the site has failed
+#define OPERATION_IGNORE	-3	
 #define OPERATION_COMPLETE	1
-
+/********************************************************************************************************************
+                                              Struct Operation Starts
+****************************************************************************************************************************/
 struct operation {
- int tid ;				//Transaction ID to which this operation belongs
- int operationType ;			//The operation could be a read/write/commit/abort/dump/recover/fail operation
+ int tid ;				
+ int operationType ;			
  int operationTimestamp ;
- int transactionType ;			//Type of the transaction to which the operation belongs
+ int transactionType ;			
  int transactionTimestamp ;
  int varNo ;
  int valueToWrite ;
  int valueRead ;
- int siteNo ;					//For operations such as dump/recover/fail, site on which operation will be applied
- int operationWaitListed_tickNo ;	        //Value of tick at which operation was sent to a given site, this will be useful in timing out operations
- int operationStatusAtSites[MAX_SITES] ; 	//Every site will set its status for the operation at the appropriate array index
+ int siteNo ;					
+ int operationWaitListed_tickNo ;	        
+ int operationStatusAtSites[MAX_SITES] ; 	
  struct operation *nextOperationSite ;
  struct operation *nextOperationTM ;
 } ;
