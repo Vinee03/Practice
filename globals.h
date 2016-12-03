653dc49ba1ed3e4947dce5b7479494d5ea924801
Vinee03 <kuttyvineetha03@gmail.com>
2016-12-03 17:56:16 -0500

    Update globals.h

--- globals.h
+++ globals.h
@@ -7,30 +7,30 @@
 #include <string.h>
 #include <errno.h>
 
-#define READONLY_TRANSACTION    		0
-#define READWRITE_TRANSACTION   		1
-#define MISCELLANEOUS_TRANSACTION		2	 
+#define RONLY_TRANSACTIONS    		0
+#define READ_WRITE_TRANSACTIONS   		1
+#define OTHER_TRANSACTIONS		2	 
 
-#define MAX_SITES		11	
-#define MAX_VARIABLES		21	
+#define MAXIMUM_SITES		11	
+#define MAXIMUM_VARIABLES		21	
 
 #define ALL_VARIABLES		100	
 #define ALL_SITES		100	
 
-#define READ_OPERATION		0
-#define WRITE_OPERATION		1
-#define END_OPERATION		2
-#define ABORT_OPERATION		-2
-#define DUMP_OPERATION		3
-#define FAIL_OPERATION		-4
-#define RECOVER_OPERATION	4
-#define QUERY_STATE_OPERATION	5
+#define READ_OPN		0
+#define WRITE_OPN		1
+#define END_OPN		2
+#define ABORT_OPN		-2
+#define DUMP_OPN		3
+#define FAIL_OPN		-4
+#define RECOVER_OPN	4
+#define QUERY_STATE_OPN	5
 
-#define OPERATION_PENDING	0
-#define OPERATION_BLOCKED	-1
-#define OPERATION_REJECTED	-2
-#define OPERATION_IGNORE	-3	
-#define OPERATION_COMPLETE	1
+#define OPN_PENDING	0
+#define OPN_BLOCKED	-1
+#define OPN_REJECTED	-2
+#define OPN_IGNORE	-3	
+#define OPN_COMPLETE	1
 /********************************************************************************************************************
                                               Struct Operation Starts
 ****************************************************************************************************************************/
@@ -45,7 +45,7 @@ struct operation {
  int valueRead ;
  int siteNo ;					
  int operationWaitListed_tickNo ;	        
- int operationStatusAtSites[MAX_SITES] ; 	
+ int operationStatusAtSites[MAXIMUM_SITES] ; 	
  struct operation *nextOperationSite ;
  struct operation *nextOperationTM ;
 } ;
